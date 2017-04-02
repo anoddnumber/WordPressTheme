d34e9d422c9591434933cf1b3b81ce580fc71228
anoddnumber <suchanoddnumber@gmail.com>
2017-04-01 23:54:49 -0700

    Fix initialization of sidr

---
+++ footer.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the "site-content" div and all content after.
+ * This is needed or else sidr won't start up
+ */
+?>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
