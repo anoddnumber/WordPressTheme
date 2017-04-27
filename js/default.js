f83678ba60967cd164278894bc99b71e1975eb9f
anoddnumber <suchanoddnumber@gmail.com>
2017-04-26 21:34:00 -0700

    Sidebar works!

--- js/default.js
+++ js/default.js
@@ -3,5 +3,10 @@ $(document).ready(function() {
   console.log("hooking up hamburger menu");
   $('.nav-trigger').on('click', function() {
     $('.bar').toggleClass('animate');
+    if($(".wrapper").hasClass('is-open')){
+      $(".wrapper").removeClass("is-open");
+    } else {
+      $(".wrapper").addClass("is-open");
+    }
   })
 });
